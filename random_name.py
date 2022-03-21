5260fa78188cad1940d366677fa6b2585f32e34f
Eerli Semsen <42339518+Yoorse@users.noreply.github.com>
2022-03-21 08:56:59 +0100

    Update random_name.py
    Rettelser af redundate linier

--- random_name.py
+++ random_name.py
@@ -4,16 +4,17 @@ from names import *
 
 def generer_navne():
 
-	random.shuffle(fornavne_list)
-	random.shuffle(efternavne_list)
+	#randomiserer navne og fornavne
+	#random.shuffle(fornavne_list)
+	#random.shuffle(efternavne_list)
 
 	navne = []
 
-
-
+	#Valg af random navn
 	randomnavn = random.choice(fornavne_list)
 	randonefternavn = random.choice(efternavne_list)
 
+	#Sammenlægning af randomnavn og randomefternavn
 	navne = randomnavn + " " + randonefternavn
 
 	print(navne) 
